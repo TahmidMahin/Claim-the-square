36868936b876f990eb472bffc03b223fec91a4a5
Tahmid <tahmidmahin1729@gmail.com>
2022-08-23 20:08:11 +0600

    TODO: 1. BOSS AI 2. fix Player1 and Player2 AI bugs 3. Grid drawing and invalid block implementation

--- src/test/java/Player1.java
+++ src/test/java/Player1.java
@@ -1,5 +1,3 @@
-import com.codingame.game.CellState;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -17,14 +15,26 @@ public class Player1 {
         List<String> moves = new ArrayList<>();
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 7; j++) {
-
+                if (grid[i][j] == index) {
+                    for (int dir = 0; dir < 8; dir++) {
+                        int r = i + dr[dir];
+                        int c = j + dc[dir];
+                        if (grid[r][c] == 0 && dir%2 == 0) {
+                            moves.add(String.format("%d %d %s %d %d", i, j, "JUMP", r ,c));
+                        }
+                        else if (grid[r][c] == 0 && dir%2 == 1) {
+                            moves.add(String.format("%d %d %s %d %d", i, j, "REPL", r ,c));
+                        }
+                    }
+                }
             }
         }
+        int position = new Random().nextInt(moves.size());
+        System.out.println(moves.get(position));
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Random random = new Random(0);
         index = in.nextInt();
 
         while (true) {
